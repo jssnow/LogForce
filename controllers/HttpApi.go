e06709da07543e751b8bcdc520365bac47cbbd24
jhj <jhj767658181@gmail.com>
2020-04-19 23:05:22 +0800

    引入日志记录

--- controllers/HttpApi.go
+++ controllers/HttpApi.go
@@ -3,20 +3,20 @@ package controllers
 import (
 	"gin_log/services"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
-
+var Log = log.New()
 
 // 接收日志控制器
 func ReceiveLog(c *gin.Context) {
 	var jsonInputs []services.JsonInput
 	if err := c.ShouldBindJSON(&jsonInputs); err != nil {
-		c.AbortWithStatusJSON(500,gin.H{
+		c.AbortWithStatusJSON(500, gin.H{
 			"message": "日志数据解析错误!",
 		})
 		return
 	}
-
 
 	// 判断是否无数据
 	if len(jsonInputs) == 0 {
@@ -26,6 +26,7 @@ func ReceiveLog(c *gin.Context) {
 		return
 	}
 	services.DealLogs(jsonInputs)
+	Log.Info("qeqweqe")
 
 	c.JSON(200, gin.H{
 		"message": "成功!",
--- routers/router.go
+++ routers/router.go
@@ -1,12 +1,37 @@
 package routers
 
 import (
+	"fmt"
 	"gin_log/controllers"
+	"gin_log/middleware"
 	"github.com/gin-gonic/gin"
+	"os"
+	"path"
 )
 
+func init() {
+	// 日志
+	logFilePath := "./"
+	logFileName := "log.log"
+
+	// 日志文件
+	fileName := path.Join(logFilePath, logFileName)
+
+	// 写入文件
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	controllers.Log.Out = src
+}
+
 func APIRouter() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+
+	// 设置输出
+
+	r.Use(middleware.Logger(controllers.Log))
 	r.POST("/http", controllers.ReceiveLog)
 	return r
 }
