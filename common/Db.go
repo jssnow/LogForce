a5c7881b778df8334c11917ec3bd20fdfdbb804b
姜红杰 <jianghongjie@jybdshop.cn>
2020-08-04 10:30:06 +0800

    数据库走配置

--- common/Db.go
+++ common/Db.go
@@ -1,7 +1,15 @@
 package common
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
 
 // Db
 var Db *gorm.DB
+
+func GetDbConfigString() string {
+	dbConfig := Config.GetStringMapString("db")
+	return fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", dbConfig["username"], dbConfig["password"], dbConfig["addr"], dbConfig["name"])
+}
 
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/jinzhu/gorm v1.9.12
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/viper v1.6.3
+	github.com/stretchr/objx v0.1.1 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
